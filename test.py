30d7e0d64d7d23113b789b19041ccc8f3b202b0a
patricks1 <patrickstaudt1@gmail.com>
2018-06-08 14:55:35 -0400

    defined file method

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 #test
 import os
 from centralms import catalog as cat
-from centralms import util as UT
+#from centralms import util as UT
 class testsubhalo(cat.Subhalos):
-	'''def File(self):
-		file_name=''.join([
-	'''
+	def File(self):
+		file_name=''.join(['/home/users/staudt/projects/mergers',
+			'Subhalos',
+			'.SHAMscat',str(self.sigma_smh),
+			'.smf_',self.smf_source,
+			'nsnap0_',str(self.nsnap0),
+			'.hdf5'])
+		return file_name
+
 print __file__
 realpath=os.path.realpath(__file__)
 print realpath
