b140f5918fae7d7925486fe7ffe62465f71f4e80
Patrick Staudt <staudt@harmattan.cosmo.fas.nyu.edu>
2018-06-08 14:32:43 -0400

    more os tests

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 #test
 import os
 from centralms import catalog as cat
+from centralms import util as UT
 class testsubhalo(cat.Subhalos):
 	'''def File(self):
 		file_name=''.join([
 	'''
-print os.path()
+print __file__
+realpath=os.path.realpath(__file__)
+print realpath
+print os.path.dirname(realpath)
+print UT.dat_dir()
