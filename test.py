33f3195fd3a9641f84e95e8fc170035fead66658
Patrick Staudt <staudt@harmattan.cosmo.fas.nyu.edu>
2018-06-08 16:51:45 -0400

    inherit super __init__

--- test.py
+++ test.py
@@ -2,18 +2,17 @@
 import os
 from centralms import catalog as cat
 #from centralms import util as UT
-class testsubhalo(cat.Subhalos):
-	def File(self):
-		file_name=''.join(['/home/users/staudt/projects/mergers',
-			'Subhalos',
-			'.SHAMscat',str(self.sigma_smh),
-			'.smf_',self.smf_source,
-			'nsnap0_',str(self.nsnap0),
-			'.hdf5'])
-		return file_name
-
-print __file__
-realpath=os.path.realpath(__file__)
-print realpath
-print os.path.dirname(realpath)
-print UT.dat_dir()
+class Testsubhalo(cat.Subhalos):
+    def __init__(self, sigma_smhm=0.2, smf_source='li-march',nsnap0=20,use_super_defaults=True):
+        if use_super_defaults:
+            super(self).__init__(self)
+        else:
+            super(self).__init__(self,sigma_smhm=sigma_smhm,smf_source=smf_source,nsnap0=nsnap0)
+    def File(self):
+	file_name=''.join(['/home/users/staudt/projects/mergers',
+        	'Subhalos',
+		'.SHAMscat',str(self.sigma_smh),
+		'.smf_',self.smf_source,
+		'nsnap0_',str(self.nsnap0),
+		'.hdf5'])
+	return file_name
