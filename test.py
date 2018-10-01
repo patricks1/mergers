24165071e415f805d1015d33cd847fb5cd595d0e
Patrick Staudt <staudt@harmattan.cosmo.fas.nyu.edu>
2018-10-01 01:33:52 -0400

    after creating branch

--- test.py
+++ test.py
@@ -1,18 +1,19 @@
-#test
 import os
 from centralms import catalog as cat
-#from centralms import util as UT
-class Testsubhalo(cat.Subhalos):
-    def __init__(self, sigma_smhm=0.2, smf_source='li-march',nsnap0=20,use_super_defaults=True):
+
+class Staudt_subhalos(cat.Subhalos):
+    '''
+    def __init__(self, sigma_smhm=None, smf_source=None, nsnap0=None,use_super_defaults=True):
         if use_super_defaults:
-            super(self).__init__(self)
+            super(Testsubhalo,self).__init__(sigma_smhm=0.2, smf_source='li-march',nsnap0=20)
         else:
-            super(self).__init__(self,sigma_smhm=sigma_smhm,smf_source=smf_source,nsnap0=nsnap0)
+            super(Testsubhalo,self).__init__(self,sigma_smhm=sigma_smhm,smf_source=smf_source,nsnap0=nsnap0)
+    '''
     def File(self):
-	file_name=''.join(['/home/users/staudt/projects/mergers',
-        	'Subhalos',
-		'.SHAMscat',str(self.sigma_smh),
-		'.smf_',self.smf_source,
-		'nsnap0_',str(self.nsnap0),
-		'.hdf5'])
-	return file_name
+        file_name=''.join(['/home/users/staudt/projects/mergers/dat/',
+            'Subhalos',
+            '.SHAMscat',str(self.sigma_smhm),
+            '.smf_',self.smf_source,
+            'nsnap0_',str(self.nsnap0),
+            '.hdf5'])
+        return file_name
