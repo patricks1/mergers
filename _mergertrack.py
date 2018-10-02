ee1307188b312808d09073a66131becdb7501fe6
ChangHoon Hahn <chh327@nyu.edu>
2018-10-01 18:55:39 -0700

    check whether the subhalo stays throughout

--- _mergertrack.py
+++ _mergertrack.py
@@ -6,6 +6,7 @@ import numpy as np
 from collections import Counter
 try: 
     from treepm import subhalo_io
+    from utilities import utility as wUT
 except ImportError: 
     pass
 
@@ -80,6 +81,81 @@ def merger_track(mmid, dlogm=0.5):
     return None 
 
 
+def merger_track_throughout(mmid, dlogm=0.5): 
+    ''' track the accreted halo masses 
+
+    snap5   * * m3
+            |/ 
+    snap4   * 
+            |
+    snap3   * * m2
+            |/ 
+    snap2   * 
+            | 
+    snap1   * * m1 
+            |/ 
+    snap0   * M0 
+    
+    output [m1/M0, m2/M0, m3/M0...]  to file 
+
+    '''
+    snapshots = np.arange(35) 
+
+    # read in treepm subhalos 
+    sub = subhalo_io.Treepm.read('subhalo', zis=snapshots)
+    nsub0 = len(sub[0]['ilk'])
+
+    # central at snapshot 0 
+    is_cen = (sub[0]['ilk'] == 1)
+    print('%i out of %i subhalos are centrals' % (np.sum(is_cen), nsub0))
+    # within [mmid - 0.5 dlogm, mmid + 0.5 dlogm] at snapshot 0 
+    in_mbin = (sub[0]['halo.m'] > mmid - 0.5*dlogm) & (sub[0]['halo.m'] < mmid + 0.5*dlogm)
+    print('%i out of %i subhalos are centrals' % (np.sum(is_cen & in_mbin), nsub0))
+    # has prognitor until the last snapshot 
+    prog_final = wUT.utilities_catalog.indices_tree(sub, 0, snapshot[-1], np.arange(nsub0))
+    has_prog = (prog_final >= 0)
+
+    ihalos = np.arange(nsub0)[is_cen & in_mbin & has_prog]
+    print('%i out of %i subhalos are tracked throughout the snapshots' % (len(ihalos), nsub0))
+    m_M0 = [] 
+    for isnap in range(snapshots[-1]): 
+        # indices of the parent with the highest M_max (i.e. primaries) 
+        i_primaries = sub[isnap]['par.i'][ihalos]
+        # only keep halos that have parents
+        # at snapshot=0 ~40 do not have parents
+        has_primary = (i_primaries >= 0) 
+        ihalos = ihalos[has_primary]
+        i_primaries = i_primaries[has_primary]
+        
+        # make sure indices match across snapshot i and i+1 
+        assert np.sum(ihalos - sub[isnap+1]['chi.i'][i_primaries]) == 0
+        
+        # identify halos with more than just a primary parent
+        # the few lines below is just to speed things up a bit 
+        counter = Counter(list(sub[isnap+1]['chi.i']))
+        ip1_children_dupl = np.array([ii for ii, count in counter.items() if count >1])
+        ihs = np.intersect1d(ihalos, np.array(ip1_children_dupl))
+    
+        for ih in ihs: 
+            # loop through halos with parents and store non-primary halo masses 
+            is_parent = np.where(sub[isnap+1]['chi.i'] == ih)[0]
+            i_primary = sub[isnap]['par.i'][ih]
+            notprimary = (is_parent != i_primary) 
+
+            m_M0 += list(sub[isnap+1]['halo.m'][is_parent[notprimary]] - sub[isnap]['halo.m'][ih])
+    
+        print('snapshot %i, %i m_M0s' % (isnap, len(m_M0)))
+        # keep searching through the primaries 
+        ihalos = i_primaries
+    print('%i halos have primaries in the last snapshot' % len(ihalos)) 
+    m_M0 = np.array(m_M0)
+
+    f = h5py.File('m_M0.h5', 'w') 
+    f.create_dataset('m_M0', data=m_M0) 
+    f.close() 
+    return None 
+
+
 def plotMFaccreted(): 
     ''' plot the mass funciton
     ''' 
@@ -102,7 +178,7 @@ def plotMFaccreted():
     fig = plt.figure(figsize=(6,6))
     sub = fig.add_subplot(111)
     bins = np.logspace(-3., np.log10(0.3), 15) 
-    sub.hist(m_M0, bins=bins, histtype='step', cumulative=-1)
+    sub.hist(m_M0, weights=np.repeat(1./7e4, len(m_M0)), bins=bins, histtype='step', cumulative=-1)
     sub.set_xlabel(r'$m/M_0$', fontsize=25)
     sub.set_xscale("log") 
     sub.set_xlim([1e-3, 0.3]) 
@@ -114,5 +190,6 @@ def plotMFaccreted():
 
 
 if __name__=="__main__": 
-    merger_track(12, dlogm=0.5)
+    #merger_track(12, dlogm=0.5)
+    merger_track_throughout(12, dlogm=0.5)
     #plotMFaccreted()
