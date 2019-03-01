9ebe0b33bb10d1025214b039bda8ca2bd77a3dad
Patrick Staudt <staudt@harmattan.cosmo.fas.nyu.edu>
2019-03-01 02:04:46 -0500

    going local

--- merg_rates.py
+++ merg_rates.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import datetime
+import time
 import h5py
 
 from wetzel_utils.utility.utility_catalog import indices_tree
@@ -33,6 +34,7 @@ class shamedTreepmClass(TreepmClass):
         self.seed=seed
         self.subisread=False
         self.hostisread=False
+        self.mtreebuilt=False
         if catkind=='subhalo':
             self.readsub()
         elif catkind=='halo':
@@ -59,6 +61,7 @@ class shamedTreepmClass(TreepmClass):
             f.create_dataset('Nprim0',data=out[5])
             f.create_dataset('zbeg',data=out[6])
             f.create_dataset('zend',data=out[7])
+            f.create_dataset('zs',data=out[8])
 
     def merg_rates_z(self,M,zibeg,ziend,N=None):
         if not self.hostisread:
@@ -168,9 +171,91 @@ class shamedTreepmClass(TreepmClass):
                 mMs+=list(mMs_add)
         return mMs,Nhost,zbeg,zend
 
+    def merg_tree(self,zibeg,ziend):
+        #Get list of m_*/M_* 
+        #M0cond can be based on either galaxy or halo mass at z=0.
+        #t0=time.time()
+        if not self.subisread:
+            self.readsub()
+        zis=np.arange(zibeg,ziend+1)
+        #print zis[:0:-1]
+        zbeg=self.subcat.snap[zibeg][1]
+        zend=self.subcat.snap[ziend][1]
+        #i_primaries0=self.get_primaries0(condtype,M0cond)
+        #i0s=np.arange(len(self.subcat[zis[-1]][self.gmtype]))
+
+        '''
+        print'%i galaxies'%len(i0s)
+        if not N is None:
+            random.seed(1)
+            i0s=random.sample(i0s,N)
+        N0=len(i0s)
+        print'evaluating %i'%N0
+        ''' 
+        
+        #pbar=ProgressBar()
+        #Evaluate zis in reverse order from highest redshift through second to
+        #lowest redshift:
+        #the second lowest:
+        for zi in zis[:0:-1]:
+        #for zi in pbar(zis[:0:-1]):
+            self.subcat[zi-1]['par.tree']={}
+            his=np.arange(len(self.subcat[zi][self.gmtype]))
+            #i_primaries=indices_tree(self.subcat,zis[0],zi,i0s)
+
+            #isolate primaries that exist and have mass at zi:
+            #i_primaries=i_primaries[i_primaries>=0.]
+            chiis=indices_tree(self.subcat,zi,zi-1,his)
+            his=his[chiis>=0]
+            chiis=chiis[chiis>=0]
+            m_chis=self.subcat[zi-1][self.gmtype][chiis]
+            ms=self.subcat[zi][self.smtype][his]
+            his=his[(ms>0.) & (m_chis>0.)]
+            
+            print'{0:d} galaxies in snapshot {1:d}'.format(len(his),zi)
+
+            for hi,chii in zip(his,chiis):
+                #print hi
+                '''
+                This shouldn't be necessary given that I'm removing primaries
+                where hi<0, so I'm commenting it. I'll delete it after testing.
+                #if the parent doesn't exist at zi, go to the next parent:
+                if hi<0: 
+                    continue
+                '''
+                #chii=self.subcat[zi]['chi.i'][hi]
+                #prim_chii=indices_tree(self.subcat,zi,zi-1,hi)
+                #isolate the accreted galaxies:
+                #notprim=chii!=prim_chii
+                #chii=chii[notprim]
+                
+                '''
+                if hi==6877:
+                    print zi
+                    print hi
+                    print chii
+                    print self.subcat[zi-1]['par.i'][chii]
+                    print hi==self.subcat[zi-1]['par.i'][chii]
+                '''
+                if hi==self.subcat[zi-1]['par.i'][chii]: 
+                    continue
+                #print'it worked'
+                #if len(chii)==0:
+                #    continue
+                if chii not in self.subcat[zi-1]['par.tree']:
+                    self.subcat[zi-1]['par.tree'][chii]=list([hi])
+                else:
+                    self.subcat[zi-1]['par.tree'][chii]+=hi
+        self.mtreebuilt=True
+
+    def gal_mMs_fromtree(self,M0cond,condtype,zibeg,ziend,Mtime,N=None):
+        if not self.mtreebuilt:
+            self.merg_tree(zibeg,ziend)
+
     def gal_mMs(self,M0cond,condtype,zibeg,ziend,Mtime,N=None):
         #Get list of m_*/M_* 
         #M0cond can be based on either galaxy or halo mass at z=0.
+        #t0=time.time()
         if not self.subisread:
             self.readsub()
         zis=np.arange(zibeg,ziend+1)
@@ -179,6 +264,7 @@ class shamedTreepmClass(TreepmClass):
         mMs=[]
         ms=[]
         Ms=[]
+        zs=[]
         iprim0s_merg=[]
         
         i_primaries0=self.get_primaries0(condtype,M0cond)
@@ -193,11 +279,31 @@ class shamedTreepmClass(TreepmClass):
         pbar=ProgressBar()
         for i_primary0 in pbar(i_primaries0):
             out=self.mMs_add_gal_f(i_primary0,zis,Mtime)
+            #print out
             mMs+=out[0]
             ms+=out[1]
             Ms+=out[2]
+            zs+=out[3]
             iprim0s_merg+=list(np.repeat(i_primary0,len(out[0])))
-        return mMs,ms,Ms,iprim0s_merg,i_primaries0,Nprim0,zbeg,zend
+        #t1=time.time()
+        #print'run time: {0}'.format(t1-t0)
+        return mMs,ms,Ms,iprim0s_merg,i_primaries0,Nprim0,zbeg,zend,zs
+
+    '''
+    def quench_dat(mus,mMs,iprim0_merg,iprim0_keys):
+        lmuwid=0.01 #log width of the merger ratio
+        lmus=np.log10(np.array(mus))
+        iscen=self.subcat[0][ilk]==1
+        M0s=self.subcat[0][gmtype][iscen]
+        minM0=np.min(M0s)
+        print'min: %f'%minM0
+        maxM0=np.max(M0s)
+        print'max: %f'%maxM0
+        Mbins=np.arange(minM0,maxM0,)
+        for mu in mus:
+            Ns=[]
+            for M in Mbins:
+    '''
 
     def get_primaries0(self,condtype,M0cond):
         if condtype=='gal':
@@ -207,7 +313,7 @@ class shamedTreepmClass(TreepmClass):
         elif condtype in ['censubhal','cengal']:
             i_primaries0=self.get_cenis(M0cond,condtype)
         else:
-            raise ValueError('Condition type is "gal", "subhal", or '
+            raise ValueError('Condition type is "cengal", "gal", "subhal", or '
                              '"censubhal".')
         return i_primaries0
 
@@ -246,6 +352,7 @@ class shamedTreepmClass(TreepmClass):
         mMs=[]
         ms=[]
         Ms=[]
+        zs=[]
         zi0=zis[0]
         if Mtime=='0':
             M=self.subcat[zi0][self.gmtype][i_primary0]
@@ -253,7 +360,7 @@ class shamedTreepmClass(TreepmClass):
             #index of the main progenitor at zi:
             i_primary=indices_tree(self.subcat,zi0,zi,i_primary0)
             if i_primary<0:
-                return mMs,ms,Ms
+                return mMs,ms,Ms,zs
             #index of the main progenitor at zi+1:
             pari=self.subcat[zi]['par.i'][i_primary] 
             
@@ -267,7 +374,7 @@ class shamedTreepmClass(TreepmClass):
             #0-M0 shows up so far on the left of the plot that we never noticed
             #it.)
             if Mz==0:
-                return mMs,ms,Ms
+                return mMs,ms,Ms,zs
             
             if Mtime=='z':
                 M=Mz #When we're doing m/Mz, set the denominator to Mz.
@@ -277,10 +384,15 @@ class shamedTreepmClass(TreepmClass):
             ms_add=self.subcat[zi+1][self.gmtype][is_prog]
             mMs_add=ms_add-M
             Ms_add=np.repeat(M,len(ms_add))
+            z=self.subcat[zi+1].snap['z']
+            zs_add=np.repeat(z,len(ms_add))
             ms+=list(ms_add)
             Ms+=list(Ms_add)
             mMs+=list(mMs_add)
-        return mMs,ms,Ms
+            zs+=list(zs_add)
+            #print zs
+        #mMs=list(-np.abs(np.array(mMs)))
+        return mMs,ms,Ms,zs
 
     def readhost(self):
         self.hostcat=self.read(zis=self.allzis,catalog_kind='halo')
@@ -293,6 +405,7 @@ class shamedTreepmClass(TreepmClass):
         self.subcat=self.read(zis=self.allzis,catalog_kind='subhalo')
         sham.assign(self.subcat,scat=self.scat,dis_mf=self.dis_mf,
                     source=self.source,
+                    #sham_prop=self.smtype,zis=self.shamzis)
                     sham_prop=self.smtype,zis=self.shamzis,seed=self.seed)
         self.subisread=True
 
@@ -336,15 +449,15 @@ class shamedTreepmClass(TreepmClass):
 
 ###############################################################################
 
-def read_galmMs(Mcond,condtype,Mtime,scat,tstmp,seed=None):
+def read_galmMs(Mcond,condtype,Mtime,scat,tstmp,seed=None,apnd=''):
     Mtime=str(Mtime)
     if (seed is None) or (not scat):
         seedstr=''
     else:
         seedstr='_'+str(seed)+'seed'
     fname='/home/users/staudt/projects/mergers/dat/' \
-          'gal_mM{4}_M{3}0_{0:0.2f}_scat{2:0.2f}{5}_{1}.h5' \
-          .format(Mcond,tstmp,scat,condtype,Mtime,seedstr)
+          'gal_mM{4}_M{3}0_{0:0.2f}_scat{2:0.2f}{5}_{1}{6}.h5' \
+          .format(Mcond,tstmp,scat,condtype,Mtime,seedstr,apnd)
     print fname
     with h5py.File(fname,'r') as f:
         mMs=np.array(f['mMs'])
@@ -355,43 +468,71 @@ def read_galmMs(Mcond,condtype,Mtime,scat,tstmp,seed=None):
         Nprim0=(f['Nprim0']).value
         zbeg=(f['zbeg']).value
         zend=(f['zend']).value
-    return mMs,ms,Ms,iprim0s_merg,iprim0s_keys,Nprim0,zbeg,zend
+        #Using this for now because of a typo in the mMswrite code. Change
+        #later:
+        zs=np.array(f['za'])
+        #zs=np.array(f['zs'])
+    return mMs,ms,Ms,iprim0s_merg,iprim0s_keys,Nprim0,zbeg,zend,zs
 
-def write_gal_hgram_dat(Mconds,condtype,Mtime,rngs,scat,mMs_tstmp,seed=None,apnd=''):
+def write_gal_hgram_dat(Mconds,condtype,Mtime,rngs,zmax,
+                        scat,mMs_tstmp,seed=None,
+                        readapnd='',apnd='',tstmp0=None):
     Mtime=str(Mtime)
     if (seed is None) or (not scat):
         seedstr=''
     else:
         seedstr='_'+str(seed)+'seed'
+    if tstmp0 is None:
+        tstmt0=mMs_tstmp
     tstmp='{:%Y%m%d}'.format(datetime.datetime.now())
     fname='./dat/gal_hgram_dat{2}_{0}{1}.h5'.format(tstmp,apnd,seedstr)
     with h5py.File(fname,'w') as f: 
         dat=[]
         Nprim0s=[]
         for Mcond in Mconds:
-            mMsdat_0scat=read_galmMs(Mcond,condtype,Mtime,0,mMs_tstmp,seed)
-            mMsdat_scat=read_galmMs(Mcond,condtype,Mtime,scat,mMs_tstmp,seed)
+            mMsdat_0scat=read_galmMs(Mcond,condtype,Mtime,0,tstmp0,seed)
+            mMsdat_scat=read_galmMs(Mcond,condtype,Mtime,scat,mMs_tstmp,seed,
+                                    readapnd)
             Mcond_dat=[]
             pbar=ProgressBar()
             for rng in pbar(rngs):
-                Ns_0scat=gal_hgram_dat(rng,mMsdat_0scat[0],
+                Ns_0scat=gal_hgram_dat_noz(rng,mMsdat_0scat[0],
                                        mMsdat_0scat[3],
                                        mMsdat_0scat[4])
                 Ns_scat=gal_hgram_dat(rng,mMsdat_scat[0],
                                       mMsdat_scat[3],
-                                      mMsdat_scat[4])
+                                      mMsdat_scat[4],
+                                      zmax,mMsdat_scat[8])
                 dname0='Ns_{0:0.1f}M_{1:0.1f}-{2:0.1f}_0scat'\
                                  .format(Mcond,rng[0],rng[1])
                 dname='Ns_{0:0.1f}M_{1:0.1f}-{2:0.1f}_scat'\
                                  .format(Mcond,rng[0],rng[1])
                 f.create_dataset(dname0,data=Ns_0scat)
                 f.create_dataset(dname,data=Ns_scat)
-            Nprim0s.append([mMsdat_scat[5],mMsdat_scat[5]])
+            #number of primary ending galaxies for the 0scat and scat data:
+            Nprim0s.append([mMsdat_0scat[5],mMsdat_scat[5]])
         f.create_dataset('Nprim0s',data=Nprim0s)
         f.create_dataset('Mconds',data=Mconds)
         f.create_dataset('rngs',data=rngs)
 
-def gal_hgram_dat(rng,mMs,iprim0s_merg,iprim0s_keys):
+def write_quench_dat(condtype,Mtime,mus,scat,mMs_tstmp):
+    #unfinished function. Might delete later.
+    lmuwid=0.01 #log width of the merger ratio
+    lmus=np.log10(np.array(mus))
+
+def gal_hgram_dat(rng,mMs,iprim0s_merg,iprim0s_keys,zmax,zs):
+    #Generate a list of the number of times a merger happens within the 
+    #given range. Each datapoint represents the merger count for a single
+    #main progenitor.
+    mMlo=rng[0]
+    mMhi=rng[1]
+    inrange=(mMs>mMlo)&(mMs<mMhi)&(zs<=zmax)
+    Ns=np.array([np.sum(ismp & inrange)
+                 for ismp in
+                 (iprim0s_merg==key for key in iprim0s_keys)])
+    return Ns
+
+def gal_hgram_dat_noz(rng,mMs,iprim0s_merg,iprim0s_keys):
     #Generate a list of the number of times a merger happens within the 
     #given range. Each datapoint represents the merger count for a single
     #main progenitor.
@@ -402,4 +543,4 @@ def gal_hgram_dat(rng,mMs,iprim0s_merg,iprim0s_keys):
                  for ismp in
                  (iprim0s_merg==key for key in iprim0s_keys)])
     return Ns
-    
+   
