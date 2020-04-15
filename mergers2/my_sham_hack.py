690ff329c49c8dc718b17b96dee7b297255eac02
Patrick Staudt <staudt@harmattan.cosmo.fas.nyu.edu>
2020-04-15 01:46:12 -0400

    figured out that a dN(z)/dz plot in testing_hash_method was jagged because it was a sample of only 2000 central subhalos. I made of note of that in testing_hash_method.

--- mergers2/my_sham_hack.py
+++ mergers2/my_sham_hack.py
@@ -138,7 +138,7 @@ class SMFClass:
             '''
             Li & White 2009. z = 0.1 from SDSS. Chabrier IMF. Complete to 1e8 M_sun/h^2.
             '''
-            self.redshifts = np.array([0.1])
+            self.redshifts = np.array([0.1,0.5])
             self.mchars = np.array([10.525]) - 2 * log10(hubble)    # {M_sun}
             self.amplitudes = np.array([0.0083]) * hubble ** 3    # {Mpc ^ -3 / log(M/M_sun)}
             self.slopes = np.array([-1.155])
